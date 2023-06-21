c2123fa8ffa28d9f4123f04f952a9931ba90104d
Erick Cohen <erickdcohen@gmail.com>
2023-06-21 14:56:45 -0400

    Adds intro to chapter 7 DOM manipulation

---
+++ ch_07_DOMManipulation/01-DOM_intro/script.js
@@ -0,0 +1,7 @@
+// console.log(window)
+// console.dir(window.document)
+// console.log(document.links[0])
+console.log(document.getElementById('main'))
+const main = document.getElementById('main')
+// main.innerHTML = '<h1>Hello from JS!</h1>'
+document.querySelector('#main h1').innerText = 'Hi!'
