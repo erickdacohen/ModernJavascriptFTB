94b46b4b9658bba7debe7d87a33b87ea9dd8d1de
Erick Cohen <erickdcohen@gmail.com>
2023-07-13 19:14:21 -0400

    Adds swiper to flixx movie app project

--- ch_12_flixx_app/flixx-app/js/script.js
+++ ch_12_flixx_app/flixx-app/js/script.js
@@ -185,7 +185,7 @@ async function displayShowDetails() {
 	  <ul class="list-group">
 		${show.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
 	  </ul>
-	  <a href="${show.homepage}" target="_blank" class="btn">Visit show Homepage</a>
+	  <a href="${show.homepage}" target="_blank" class="btn">Visit Show Homepage</a>
 	</div>
   </div>
   <div class="details-bottom">
@@ -231,6 +231,52 @@ function displayBackgroundImage(type, backgroundPath) {
 	} else {
 		document.querySelector('#show-details').appendChild(overlayDiv)
 	}
+}
+
+// Display Slider Movies
+async function displaySlider() {
+	const { results } = await fetchAPIData('movie/now_playing')
+
+	results.forEach((movie) => {
+		const div = document.createElement('div')
+		div.classList.add('swiper-slide')
+
+		div.innerHTML = `
+		<a href="movie-details.html?id=${movie.id}">
+			<img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
+		</a>
+		<h4 class="swiper-rating">
+			<i class="fas fa-star text-secondary"></i> ${movie.vote_average} / 10
+		</h4>
+		`
+		document.querySelector('.swiper-wrapper').appendChild(div)
+
+		initSwiper()
+	})
+}
+
+function initSwiper() {
+	const swiper = new Swiper('.swiper', {
+		slidesPerView: 1,
+		spaceBetween: 30,
+		freeMode: true,
+		loop: true,
+		autoplay: {
+			delay: 4000,
+			disableOnInteraction: false,
+		},
+		breakpoints: {
+			500: {
+				slidesPerView: 2,
+			},
+			700: {
+				slidesPerView: 3,
+			},
+			1200: {
+				slidesPerView: 4,
+			},
+		},
+	})
 }
 
 // Fetch data from TMDB API
@@ -281,6 +327,7 @@ function init() {
 		case '/':
 		case '/index.html':
 			displayPopularMovies()
+			displaySlider()
 			break
 		case '/shows.html':
 			displayPopularShows()
